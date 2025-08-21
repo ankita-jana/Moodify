9ff39c63f7c859da2d65b132911b44022d8b07e2
Ankita Jana <ankitaa01.jana@gmail.com>
2025-08-21 19:03:47 +0530

    Update api.js

--- client/src/api.js
+++ client/src/api.js
@@ -1,8 +1,5 @@
 import axios from "axios";
 
-const API_BASE_URL =
-  import.meta.env.MODE === "development"
-    ? "http://localhost:5000"            
-    : "https://moodify-8.onrender.com";  
+const API_BASE_URL = import.meta.env.VITE_API_BASE_URL;
 
 export default axios.create({ baseURL: API_BASE_URL });
